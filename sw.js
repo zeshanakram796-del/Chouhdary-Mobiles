278803a8485304364fd43e9d68f7df61c3be27a2
zeshanakram796-del <zeshanakram796@gmail.com>
2026-08-25 11:24:41 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'cm-v1';
+const ASSETS = [
+  './',
+  './chouhdary-mobiles.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
